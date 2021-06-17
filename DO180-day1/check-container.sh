ede57dfc0bf34368397e2386a1b52b9a8158ff40
Shubham Katara <shubham.katara59@gmail.com>
2021-06-17 07:26:36 +0530

    Update check-container.sh

--- DO180-day1/check-container.sh
+++ DO180-day1/check-container.sh
@@ -2,7 +2,7 @@
 
 curl $(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mynginx) | grep "You guys are amazing for learning containers and OpenShift with DO180. Adios. "
 
-if [ $? eq 0 ]; then
+if [ $? -eq 0 ]; then
 	echo "Congratulations. You have passed the text. :)"
 fi
 
